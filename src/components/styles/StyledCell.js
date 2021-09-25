9b6dabe8452d7144a469f4ba1369985476869353
zhangyiqi <yiqi@animo.ai>
2021-09-25 11:37:40 -0400

    rotation and drop and collision detection working

--- src/components/styles/StyledCell.js
+++ src/components/styles/StyledCell.js
@@ -2,8 +2,6 @@ import styled from "styled-components";
 
 export const StyledCell = styled.div`
   width: auto;
-  min-height: 1rem;
-  min-width: 1rem;
   background: rgba(${(props) => props.color}, 0.8);
   border: ${(props) => (props.type === 0 ? "0px solid" : "4px solid")};
   border-bottom-color: rgba(${(props) => props.color}, 0.1);
